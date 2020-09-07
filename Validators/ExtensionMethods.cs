e38dbe78f2933aaa05483bcb625e64eebdc73064
Etienne Charland <mysteryx93@protonmail.com>
2020-09-06 23:03:09 -0500

    Added GetFlags extension method

--- Validators/ExtensionMethods.cs
+++ Validators/ExtensionMethods.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
 
 namespace HanumanInstitute.Validators
 {
     public static class ExtensionMethods
     {
+        /// <summary>
+        /// Returns a list of all enumeration flags that are contained in a value.
+        /// </summary>
+        /// <typeparam name="T">The enumeration type.</typeparam>
+        /// <param name="value">The value to return the individual flags for.</param>
+        /// <returns>A list of single-byte enumeration flags.</returns>
+        public static IEnumerable<T> GetFlags<T>(this T value)
+            where T : Enum
+        {
+            var valueLong = Convert.ToUInt64(value, CultureInfo.InvariantCulture);
+            foreach (var flag in value.GetType().GetEnumValues().Cast<T>())
+            {
+                if (
+                    // enumValue is T flag // cast enumValue to T &&
+                    Convert.ToUInt64(flag, CultureInfo.InvariantCulture) is var bitValue // convert flag to ulong
+                    && (bitValue & (bitValue - 1)) == 0 // is this a single-bit value?
+                    && (valueLong & bitValue) != 0 // is the bit set?
+                   )
+                {
+                    yield return flag;
+                }
+            }
+        }
+
         /// <summary>
         /// Copies all fields from one instance of a class to another.
         /// </summary>
